803d724d2966eba52ebf67d58849a45aeac1292a
Siryou <siryoucode@gmail.com>
2024-08-21 12:30:51 +0900

    Deploy build files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import './assets/main.scss'
+import '@/assets/main.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import pinia from './stores'
+import pinia from '@/stores'
 
 const app = createApp(App)
 
